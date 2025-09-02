fb0a90edddaf9113f34fadf40fd068a6ce37332f
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-01 22:00:06 -0500

    feat: update index (frontend o12.1)

--- desarrollo-frontend/o12-nodejs-y-npm/peruvian-food-api/src/index.js
+++ desarrollo-frontend/o12-nodejs-y-npm/peruvian-food-api/src/index.js
@@ -1,19 +1,29 @@
+import cors from 'cors';
 import express from 'express';
 import foodRouter from './routes/foodRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (_, res) => {
+app.use(cors());
+
+app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the Peruvian Food API',
+    message: "Welcome to the Peruvian Food API",
     endpoints: {
-      getAllFoods: '/api/foods'
-    },
+      getAllFoods: "/api/foods",
+      getFoodById: "/api/foods/1",
+      filterFoods: "/api/foods?search=chi",
+      combinedFilters: "/api/foods?search=gallina&category=main&isSpicy=true"
+    }
   });
 });
 
 app.use('/api/foods', foodRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
