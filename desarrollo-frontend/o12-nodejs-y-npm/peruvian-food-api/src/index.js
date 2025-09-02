b7065ef57a758df32225375f1b357bc153cb4af2
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-01 21:43:32 -0500

    feat: update index with food (frontend o12.1)

--- desarrollo-frontend/o12-nodejs-y-npm/peruvian-food-api/src/index.js
+++ desarrollo-frontend/o12-nodejs-y-npm/peruvian-food-api/src/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import foodRouter from './routes/foodRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'The server is running!' });
+app.get('/', (_, res) => {
+  res.json({
+    message: 'Welcome to the Peruvian Food API',
+    endpoints: {
+      getAllFoods: '/api/foods'
+    },
+  });
 });
+
+app.use('/api/foods', foodRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
