b1adc02a0394a8eb8d128210ce874d57abd8b8ed
elliotgaramendi <elliotgaramendi@gmail.com>
2025-09-01 21:59:41 -0500

    feat: update foods routes (frontend o12.1)

--- desarrollo-frontend/o12-nodejs-y-npm/peruvian-food-api/src/routes/foodRoutes.js
+++ desarrollo-frontend/o12-nodejs-y-npm/peruvian-food-api/src/routes/foodRoutes.js
@@ -10,16 +10,26 @@ foodRouter.get('/', (req, res) => {
   if (search) {
     filteredFoods = filteredFoods.filter(food => food.name.toLowerCase().includes(search.toLowerCase()));
   }
-
   if (category) {
     filteredFoods = filteredFoods.filter(food => food.category === category);
   }
-
   if (isSpicy !== undefined) {
     filteredFoods = filteredFoods.filter(food => food.isSpicy === (isSpicy === 'true'));
   }
 
   res.json(filteredFoods);
+});
+
+foodRouter.get('/:id', (req, res) => {
+  const id = +req.params.id;
+
+  const food = foods.find(food => food.id === id);
+
+  if (!food) {
+    return res.status(404).json({ message: 'Food not found' });
+  }
+
+  res.json(food);
 });
 
 export default foodRouter;
