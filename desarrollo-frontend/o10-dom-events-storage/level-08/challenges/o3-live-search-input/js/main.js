6e584990593ed1ab0bdbb48c55c6911f28f9b4c3
elliotgaramendi <elliotgaramendi@gmail.com>
2025-12-02 21:12:10 -0500

    feat: add base code (frontend o10 JS lvl 8.3)

---
+++ desarrollo-frontend/o10-dom-events-storage/level-08/challenges/o3-live-search-input/js/main.js
@@ -0,0 +1,13 @@
+const heroes = ['Elliot', 'Fernanda', 'Fe', 'Mijael', 'Doky', 'Amorosa', 'Chocolate']; // array de héroes 🦸
+
+// your code here 💻
+// select searchInput and resultsList
+// display all heroes initially
+// add input event listener
+// filter heroes and update list
+
+console.log(resultsList.children.length === 2);
+console.log(heroes.length === 7);
+console.log(resultsList.children.length === 1);
+console.log(searchInput instanceof HTMLInputElement === true);
+console.log(typeof heroes === 'object');
