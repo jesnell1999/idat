4ce361552ca16ecffca9bde86fab38f71b6a9b5e
elliotgaramendi <elliotgaramendi@gmail.com>
2025-12-02 21:31:48 -0500

    feat: add final code (frontend o10 JS lvl 8.3)

--- desarrollo-frontend/o10-dom-events-storage/level-08/challenges/o3-live-search-input/js/main.js
+++ desarrollo-frontend/o10-dom-events-storage/level-08/challenges/o3-live-search-input/js/main.js
@@ -1,13 +1,33 @@
-const heroes = ['Elliot', 'Fernanda', 'Fe', 'Mijael', 'Doky', 'Amorosa', 'Chocolate']; // array de héroes 🦸
+const heroes = ['Elliot', 'Fernanda', 'Fe', 'Mijael', 'Doky', 'Amorosa', 'Chocolate'];
 
-// your code here 💻
-// select searchInput and resultsList
-// display all heroes initially
-// add input event listener
-// filter heroes and update list
+const searchInput = document.getElementById('searchInput');
+const resultsList = document.getElementById('resultsList')
 
-console.log(resultsList.children.length === 2);
-console.log(heroes.length === 7);
-console.log(resultsList.children.length === 1);
-console.log(searchInput instanceof HTMLInputElement === true);
-console.log(typeof heroes === 'object');
+const renderHeroes = (heroesArray) => {
+  resultsList.innerHTML = '';
+
+  heroesArray.forEach(hero => {
+    resultsList.innerHTML += `<li class="results__item">${hero}</li>`;
+  });
+};
+
+searchInput.addEventListener('input', (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+
+  const filteredHeroes = heroes.filter(hero =>
+    hero.toLowerCase().includes(searchTerm)
+  );
+
+  renderHeroes(filteredHeroes);
+  test();
+});
+
+renderHeroes(heroes);
+
+const test = () => {
+  console.log(resultsList.children.length === 2);
+  console.log(heroes.length === 7);
+  console.log(resultsList.children.length === 1);
+  console.log(searchInput instanceof HTMLInputElement === true);
+  console.log(typeof heroes === 'object');
+};
