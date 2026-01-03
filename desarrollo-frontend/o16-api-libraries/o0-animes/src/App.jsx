608defdce0fc7c06f8fdb72546de01609682dd9d
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-02 21:51:02 -0500

    chore: configure react-router (frontend o16.0)

--- desarrollo-frontend/o16-api-libraries/o0-animes/src/App.jsx
+++ desarrollo-frontend/o16-api-libraries/o0-animes/src/App.jsx
@@ -1,18 +1,12 @@
-import CardGrid from './components/CardGrid'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <CardGrid />
-      </main>
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/animes" element={<h1>Animes</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
+    </Routes>
   )
 }
 
--- desarrollo-frontend/o16-api-libraries/o0-animes/src/main.jsx
+++ desarrollo-frontend/o16-api-libraries/o0-animes/src/main.jsx
@@ -2,10 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
