f956de75960b78a8b8c3a1ad4d74c1dacd288830
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-02 21:52:40 -0500

    feat: add Animes page (frontend o16.0)

--- desarrollo-frontend/o16-api-libraries/o0-animes/src/App.jsx
+++ desarrollo-frontend/o16-api-libraries/o0-animes/src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router'
+import Animes from './pages/Animes'
 import Home from './pages/Home'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/animes" element={<h1>Animes</h1>} />
+      <Route path="/animes" element={<Animes />} />
       <Route path="/contact" element={<h1>Contact</h1>} />
     </Routes>
   )
---
+++ desarrollo-frontend/o16-api-libraries/o0-animes/src/pages/Animes.jsx
@@ -0,0 +1,17 @@
+import CardGrid from '../components/CardGrid';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+const Animes = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <CardGrid />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Animes;
