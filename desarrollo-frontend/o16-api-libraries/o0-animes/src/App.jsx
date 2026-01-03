2d339d94d164be6efd4c7842805595ca22809440
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-02 21:37:13 -0500

    feat: add Footer component (frontend o16.0)

--- desarrollo-frontend/o16-api-libraries/o0-animes/src/App.jsx
+++ desarrollo-frontend/o16-api-libraries/o0-animes/src/App.jsx
@@ -1,4 +1,5 @@
 import CardGrid from './components/CardGrid'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -10,6 +11,7 @@ function App() {
         <Hero />
         <CardGrid />
       </main>
+      <Footer />
     </>
   )
 }
