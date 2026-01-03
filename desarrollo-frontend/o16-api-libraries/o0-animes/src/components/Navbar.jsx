51fdb2db5f5fa5b58d2e68f8b3b111a307cb72f4
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-02 21:52:06 -0500

    feat: update Navbar with Link (frontend o16.0)

--- desarrollo-frontend/o16-api-libraries/o0-animes/src/components/Navbar.jsx
+++ desarrollo-frontend/o16-api-libraries/o0-animes/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
+import { Link } from 'react-router';
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <Link className="navbar-brand" to="/">Anidat</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <Link className="nav-link active" aria-current="page" to="/animes">Animes</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <Link className="nav-link" to="/contact">Contact</Link>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
