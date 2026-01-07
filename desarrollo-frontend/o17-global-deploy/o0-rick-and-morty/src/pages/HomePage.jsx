ae24d28d5b5f67f30a1e6fdb3b417791593f01f9
elliotgaramendi <elliotgaramendi@gmail.com>
2026-01-06 22:30:46 -0500

    feat: update Home page (frontend o17.0)

--- desarrollo-frontend/o17-global-deploy/o0-rick-and-morty/src/pages/HomePage.jsx
+++ desarrollo-frontend/o17-global-deploy/o0-rick-and-morty/src/pages/HomePage.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
+import Characters from '../components/Characters';
+import Hero from '../components/Hero';
+import { getCharacters } from '../services/rickandmorty';
+
 const HomePage = () => {
+  const [charactersData, setCharactersData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCharacters();
+      setCharactersData(data);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(charactersData);
+  }, [charactersData]);
+
   return (
-    <h1>HomePage</h1>
+    <>
+      <Hero />
+      <Characters data={charactersData} />
+    </>
   );
 };
 
